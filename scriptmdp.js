977b24470f610f4f201c3464e29c10ee127d744a
Robin0u <141729103+Robin0u@users.noreply.github.com>
2024-06-29 16:40:38 +0200

    Add files via upload

---
+++ scriptmdp.js
@@ -0,0 +1,13 @@
+function checkPassword() {
+    var password = document.getElementById('password').value;
+    var correctPassword = "Luidji2024";
+    var errorMessage = document.getElementById('error-message');
+    
+    if (password === correctPassword) {
+        window.location.href = "index.html";
+    } else {
+        errorMessage.style.display = 'block';
+    }
+}
+
+
